9548ca9e8610138180190a6c1782453e0110b54b
shyreckdc <shyreckdc@gmail.com>
2021-09-06 16:48:35 +0800

    add drag mode, balance mode and assembly mode in multi_axis_admittance_control_callback.m

--- tutorial/pbd/multi_axis_admittance_control_callback.m
+++ tutorial/pbd/multi_axis_admittance_control_callback.m
@@ -7,7 +7,7 @@ global gravity_compensated_force
 global current_vel_data
 persistent force_before_contact;
 persistent pos_seq;
-persistent n previous_updated_user_V_user
+persistent count_number previous_dV init_user_pose
 
 % the default control frame of ur is formed by the trans of tool frame w.r.t. the
 % base frame. The pose of the control frame is identical to the base
@@ -16,7 +16,7 @@ persistent n previous_updated_user_V_user
 % The control frame is the reference frame of the obtained vel from ur
 % script
 persistent control_frame;
-
+persistent pre_base_T_tool;
 
 % vel handle
 vel_msg = handles.vel_msg;
@@ -25,26 +25,60 @@ vel_pub = handles.vel_pub;
 % define M B K matrix in user-defined frame, for example, the frame fixed
 % on the tip of the peg
 
+% The operation mode should be chosen for different mode.
+% 0 for keep static, 1 for drag, 2 for assembly
+% The acceleration should be changed with rosparam /speedl_acceleration
+% 0.1 for keep static, 0.5 for drag, 0.2 for assembly
+
+operation_mode = 2;
+assembly_time = 20; % obtained from demonstration
 
 
-% % maintain in a static pose in space
-M = [1*eye(3) zeros(3,3);zeros(3,3) 1*eye(3)]; % omega in the first three rows
-B = [20*eye(3) zeros(3,3);zeros(3,3) 20*eye(3)];
-K = [20*eye(3) zeros(3,3);zeros(3,3) 20*eye(3)];
-
-% % drag mode
-% M = [0.1*eye(3) zeros(3,3);zeros(3,3) 0.1*eye(3)]; % omega in the first three rows
-% B = [0.2*eye(3) zeros(3,3);zeros(3,3) 0.5*eye(3)];
-% K = [0*eye(3) zeros(3,3);zeros(3,3) 0*eye(3)];
-% 
-% % assembly mode
-% M = [1*eye(3) zeros(3,3);zeros(3,3) 5*eye(3)]; % omega in the first three rows
-% B = [20*eye(3) zeros(3,3);zeros(3,3) 20*eye(3)];
-% K = [0*eye(3) zeros(3,3);zeros(3,3) 0*eye(3)];
-% % % adjust the param along z-axis
-% % B(6,6) = 2;
-% K(6,6) = 60;
-
+if operation_mode == 0
+    % % maintain in a static pose in space
+    M = [1*eye(3) zeros(3,3);zeros(3,3) 5*eye(3)]; % omega in the first three rows
+    B = [20*eye(3) zeros(3,3);zeros(3,3) 40*eye(3)];
+    K = [20*eye(3) zeros(3,3);zeros(3,3) 80*eye(3)];
+    
+    M = [1 0 0 0 0 0;
+        0 1 0 0 0 0;
+        0 0 1 0 0 0;
+        0 0 0 5 0 0;
+        0 0 0 0 5 0;
+        0 0 0 0 0 5];
+    
+    B = [2 0 0 0 0 0;
+        0 2 0 0 0 0;
+        0 0 2 0 0 0;
+        0 0 0 20 0 0;
+        0 0 0 0 20 0;
+        0 0 0 0 0 20];
+    
+    K = [10 0 0 0 0 0;
+        0 10 0 0 0 0;
+        0 0 10 0 0 0;
+        0 0 0 100 0 0;
+        0 0 0 0 100 0;
+        0 0 0 0 0 100];
+elseif operation_mode == 1
+    % drag mode
+    M = [0.1*eye(3) zeros(3,3);zeros(3,3) 0.1*eye(3)]; % omega in the first three rows
+    B = [0.2*eye(3) zeros(3,3);zeros(3,3) 0.5*eye(3)];
+    K = [0*eye(3) zeros(3,3);zeros(3,3) 0*eye(3)];
+    
+elseif operation_mode == 2
+    % assembly mode
+    M = [0.1*eye(3) zeros(3,3);zeros(3,3) 5*eye(3)]; % omega in the first three rows
+    B = [0.2*eye(3) zeros(3,3);zeros(3,3) 20*eye(3)];
+    K = [0*eye(3) zeros(3,3);zeros(3,3) 0*eye(3)];
+    % % adjust the param along z-axis
+    M(6,6) = 25;
+    B(6,6) = 130;
+    K(6,6) = 160;
+%     M(6,6) = 25;
+%     B(6,6) = 100;
+%     K(6,6) = 100;
+end
 
 % M = 1*eye(6);
 % B = 0*eye(6);
@@ -63,8 +97,8 @@ if isempty(pos_seq)
     n = 1;
 end
 
-if isempty(previous_updated_user_V_user)
-    previous_updated_user_V_user = zeros(6,1);
+if isempty(previous_dV)
+    previous_dV = zeros(6,1);
 end
 
 % get control frame
@@ -90,6 +124,7 @@ sensor_T_control = base_T_sensor \ control_frame;
 sensor_T_tool = base_T_sensor \ base_T_tool;
 
 contact_force = gravity_compensated_force - force_before_contact;
+% contact_force = gravity_compensated_force;
 
 % omit disturbance
 if max(abs(contact_force)) < 2
@@ -105,26 +140,56 @@ contact_force([1:3 4:6]) = contact_force([4:6 1:3]);
 %     static_S_theta = se3ToVec(static_se3_sensor); % the axis S is multiplied by the angle theta
 %     sensor_S_theta = Adjoint(inv(static_T_sensor))*static_S_theta;
 
+% get vel using own algorithm
+if isempty(pre_base_T_tool)
+    pre_base_T_tool = base_T_tool;
+end
+derived_pose_diff = pre_base_T_tool \ base_T_tool;
+derived_tool_se3 = MatrixLog6(derived_pose_diff);
+derived_tool_S_theta = se3ToVec(derived_tool_se3);
+derived_tool_V_tool = derived_tool_S_theta*loop_rate_hz;
+pre_base_T_tool = base_T_tool;
+
+
 % using system vel interface to get velocity
 control_V_control = current_vel_data;
 control_V_control([1:3 4:6]) = control_V_control([4:6 1:3]); % change to the form of [omega v]'
 
 
-% when tool frame and user frame overlap
-tool_T_user = eye(4);
-sensor_T_user = sensor_T_tool*tool_T_user;
-base_T_target_user = RpToTrans(rotx(pi)*rotz(pi/2),[0.111, 0.486, 0.433]');
-control_T_user = sensor_T_control \ sensor_T_user;
-
-% % custom user frame
-% % sensor_T_user = [eye(3) [0 0 0.31]'; 0 0 0 1];
-% sensor_T_user = [eye(3) [0 0 0.33]'; 0 0 0 1];
-% tool_T_user = sensor_T_tool \ sensor_T_user;
+% % when tool frame and user frame overlap
+% tool_T_user = eye(4);
+% sensor_T_user = sensor_T_tool*tool_T_user;
+% base_T_target_user = RpToTrans(rotx(pi)*rotz(pi/2),[0.111, 0.486, 0.453]');
 % control_T_user = sensor_T_control \ sensor_T_user;
-% base_T_target_user = RpToTrans(roty(pi),[0.109, 0.487, 0.05]');
-% % base_T_target_user = RpToTrans(roty(pi),[0.109, 0.487, -0.02]');
 
+% custom user frame
+% sensor_T_user = [eye(3) [0 0 0.31]'; 0 0 0 1];
+% sensor_T_user = [eye(3) [0 0 0.392]'; 0 0 0 1];  % 长杆
+sensor_T_user = [eye(3) [0 0 0.28]'; 0 0 0 1];  % 短粗杆
+tool_T_user = sensor_T_tool \ sensor_T_user;
+control_T_user = sensor_T_control \ sensor_T_user;
+% base_T_target_user = RpToTrans(roty(pi),[0.109, 0.487, 0.05]');
+% base_T_final_target_user = RpToTrans(roty(pi),[0.109, 0.487, -0.10]'); % 长杆
+base_T_final_target_user = RpToTrans(roty(pi),[0.109, 0.487, -0.032]'); % 短粗杆
+ 
 base_T_user = base_T_sensor*sensor_T_user;
+
+
+% dynamic balance pose
+if isempty(init_user_pose)
+    init_user_pose = base_T_user;
+    final_target_user_T_init_user = inv(base_T_final_target_user)*base_T_user;
+    final_target_user_se3 = MatrixLog6(final_target_user_T_init_user);
+    final_target_user_S_theta = se3ToVec(final_target_user_se3); % the angle theta has been included
+    final_target_user_S_theta_per_second = final_target_user_S_theta / assembly_time;
+end
+
+if (assembly_time - count_number/loop_rate_hz) > 0
+    base_T_target_user = base_T_final_target_user * final_target_user_S_theta_per_second * (assembly_time - count_number/loop_rate_hz);
+else
+    base_T_target_user = base_T_final_target_user;
+end
+% end dynamic balance pose
 
 target_user_T_user = inv(base_T_target_user)*base_T_user;
 target_user_se3 = MatrixLog6(target_user_T_user);
@@ -160,21 +225,55 @@ contact_force;
 tool_contact_force = Adjoint(sensor_T_tool)'*contact_force;
 tool_V_tool = Adjoint(tool_T_control)*control_V_control;
 
+% use derived vel
+tool_V_tool = derived_tool_V_tool;
+
 Ad_user_T_tool = Adjoint(inv(tool_T_user));
 M_tool = Ad_user_T_tool'*M*Ad_user_T_tool;
 B_tool = Ad_user_T_tool'*B*Ad_user_T_tool;
 K_tool = Ad_user_T_tool'*K*Ad_user_T_tool;
 
-dV = M_tool\(tool_contact_force - K_tool*tool_S_theta - B_tool*tool_V_tool)
+dV = M_tool\(tool_contact_force - K_tool*tool_S_theta - B_tool*tool_V_tool);
 % dV = M\(tool_contact_force - K*tool_S_theta - B*tool_V_tool);
+
+% % judge if dV changed quickly
+% if dV'*previous_dV < 0
+%     dV = zeros(6,1);
+% end
+%
+% previous_dV = dV;
+
+
+
+
 % compute spatial twist
-% updated_user_V_user(1:3) = (4*dV(1:3) + 0.5*user_V_user(1:3))/(loop_rate_hz);
-% updated_user_V_user(4:6) = (0.6*dV(4:6) + 0.1*user_V_user(4:6))/(loop_rate_hz);
-updated_tool_V_tool(1:3) = (1*dV(1:3)/(loop_rate_hz) + 0*tool_V_tool(1:3));
-updated_tool_V_tool(4:6) = (1*dV(4:6)/(loop_rate_hz) + 0*tool_V_tool(4:6));
-updated_tool_V_tool;
+
+
+if operation_mode == 0
+    updated_tool_V_tool(1:3) = (1*dV(1:3)/(loop_rate_hz) + 0.2*tool_V_tool(1:3));
+    updated_tool_V_tool(4:6) = (1*dV(4:6)/(loop_rate_hz) + 0.2*tool_V_tool(4:6));
+elseif operation_mode == 1
+    updated_tool_V_tool(1:3) = (1*dV(1:3)/(loop_rate_hz) + 0*tool_V_tool(1:3));
+    updated_tool_V_tool(4:6) = (1*dV(4:6)/(loop_rate_hz) + 0*tool_V_tool(4:6));
+elseif operation_mode == 2
+    updated_tool_V_tool(1:3) = (1*dV(1:3)/(loop_rate_hz) + 0.2*tool_V_tool(1:3));
+    updated_tool_V_tool(4:6) = (1*dV(4:6)/(loop_rate_hz) + 0.2*tool_V_tool(4:6));
+end
+
+if(abs(contact_force(6))>5)
+    disp("seq")
+    count_number
+    tool_contact_force
+    control_V_control
+    tool_V_tool
+    derived_tool_V_tool
+    dV
+    updated_tool_V_tool
+    disp("end")
+end
 
 control_V_control_feedback = Adjoint(inv(tool_T_control))*updated_tool_V_tool;
+
 
 
 %     tool_V_tool = Adjoint(inv(sensor_T_tool)) * updated_user_V_user;
@@ -190,26 +289,34 @@ vel_msg.Linear.Z = control_V_control_feedback(6);
 % vel_msg.Linear.Z =-0.001;
 % vel_msg.Linear.Z = 0.02;
 
-% speed limit
-% vel_msg.Linear.X = limit_speed(vel_msg.Linear.X,0.001,0.25);
-% vel_msg.Linear.Y = limit_speed(vel_msg.Linear.Y,0.001,0.25);
-% vel_msg.Linear.Z = limit_speed(vel_msg.Linear.Z,0.001,0.25);
-vel_msg.Angular.X = 0;
-vel_msg.Angular.Y = 0;
-vel_msg.Angular.Z = 0;
+% % speed limit
+vel_msg.Linear.X = limit_speed(vel_msg.Linear.X,0.001,0.25);
+vel_msg.Linear.Y = limit_speed(vel_msg.Linear.Y,0.001,0.25);
+vel_msg.Linear.Z = limit_speed(vel_msg.Linear.Z,0.001,0.25);
+% vel_msg.Angular.X = 0;
+% vel_msg.Angular.Y = 0;
+% vel_msg.Angular.Z = 0;
+% vel_msg.Linear.X = 0;
+% vel_msg.Linear.Y = 0;
+% vel_msg.Linear.Z = 0;
 vel_msg.Angular.X = limit_speed(vel_msg.Angular.X,0.001,1);
 vel_msg.Angular.Y = limit_speed(vel_msg.Angular.Y,0.001,1);
 vel_msg.Angular.Z = limit_speed(vel_msg.Angular.Z,0.001,1);
-% vel_msg.Linear.X = limit_speed(vel_msg.Linear.X,0.0005,0.25);
-% vel_msg.Linear.Y = limit_speed(vel_msg.Linear.Y,0.0005,0.25);
-% vel_msg.Linear.Z = limit_speed(vel_msg.Linear.Z,0.0005,0.25);
-% vel_msg.Angular.X = limit_speed(vel_msg.Angular.X,0.0005,1);
-% vel_msg.Angular.Y = limit_speed(vel_msg.Angular.Y,0.0005,1);
-% vel_msg.Angular.Z = limit_speed(vel_msg.Angular.Z,0.0005,1);
+
+
+
+
+
+% vel_msg.Linear.X = limit_speed(vel_msg.Linear.X,0.001,0.02);
+% vel_msg.Linear.Y = limit_speed(vel_msg.Linear.Y,0.001,0.02);
+% vel_msg.Linear.Z = limit_speed(vel_msg.Linear.Z,0.001,0.02);
+% vel_msg.Angular.X = limit_speed(vel_msg.Angular.X,0.001,0.1);
+% vel_msg.Angular.Y = limit_speed(vel_msg.Angular.Y,0.001,0.1);
+% vel_msg.Angular.Z = limit_speed(vel_msg.Angular.Z,0.001,0.1);
 
 
 send(vel_pub,vel_msg);
-n = n+1;
+count_number = count_number+1;
 
 
 
